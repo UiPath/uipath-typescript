d13efab591214a58ea719279ecb9b824bb6fe1a4
Raina451 <133850046+Raina451@users.noreply.github.com>
2025-09-04 13:59:00 +0530

    Remove dotent logs from cli output (#46)

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -4,6 +4,6 @@ import { config } from 'dotenv';
 import { run } from '@oclif/core';
 
 // Load environment variables from .env file
-config();
+config({ quiet: true });
 
 await run(process.argv.slice(2), import.meta.url);
