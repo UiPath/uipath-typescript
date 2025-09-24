4de5d88614a0ee8b48bcedfeeac4d694c499ad79
swati354 <103816801+swati354@users.noreply.github.com>
2025-09-24 11:53:07 +0530

    Add getAll for cases (#73)

--- src/models/maestro/index.ts
+++ src/models/maestro/index.ts
@@ -7,3 +7,7 @@ export * from './processes.models';
 // Process instance types and models
 export * from './process-instances.types';
 export * from './process-instances.models';
+
+// Cases types and models
+export * from './cases.types';
+export * from './cases.models';
