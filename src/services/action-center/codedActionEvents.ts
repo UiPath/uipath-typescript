47388b45dc6b35bf19ea4894d3dde1dccb2209fc
Sandeepan-Ghosh-0312 <sandeepan.ghosh@uipath.com>
2025-11-18 04:13:25 +0530

    feat: coded action app event service

---
+++ src/services/action-center/codedActionEvents.ts
@@ -0,0 +1,10 @@
+export enum EVENT_NAMES {
+  INIT = 'init',
+  COMPLETE = 'complete',
+  DATACHANGED = 'dataChanged',
+  LOADAPP = 'loadApp',
+  ERROR = 'error',
+  TOKENREFRESHED = 'tokenRefreshed',
+  LANGUAGECHANGED = 'languageChanged',
+  THEMECHANGED = 'themeChanged',
+}
