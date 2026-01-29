e1852379135d49811374fbe5b2b7a6289a7cd8de
SunilYadav-uipath <sunil.yadav@uipath.com>
2026-01-29 09:04:59 +0530

    resolve comments on models

--- src/models/conversational-agent/user/user.constants.ts
+++ src/models/conversational-agent/user/user.constants.ts
@@ -2,10 +2,11 @@
  * Constants for User Service
  */
 
+import { CommonFieldMap } from '../common.constants';
+
 /**
  * Maps fields for User Settings entities to ensure consistent SDK naming
  */
 export const UserSettingsMap: { [key: string]: string } = {
-  createdAt: 'createdTime',
-  updatedAt: 'updatedTime'
+  ...CommonFieldMap
 };
