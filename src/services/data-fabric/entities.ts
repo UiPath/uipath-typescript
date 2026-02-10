8f46215935d0e1e10eab99b7f9390b80532c21af
Mihai Dicianu <mihai.dicianu@uipath.com>
2026-02-10 08:49:30 +0200

    [PLT-96211] Add SDK method to get individual record by ID (#178)
    * feat: add uipath push command with resource import support (#164)
    
    * feat: uipath push command with solution resource import support
    
    * feat: add API guide links (#172)
    
    PLT-92717
    
    * chore: add local doc run instructions (#173)
    
    * chore: add local doc run instructions
    
    * Update README.md
    
    * Add SDK method to get individual record by ID
    
    * Fix getRecordById modular import in docs
    
    * Apply EntityMap transformations to getRecordById returned data
    
    * Update getRecordById docs example
    
    * chore:update directory (#181)
    
    * Fix whitespace
    
    * Fix oauth scopes
    
    * chore: update oauth doc
    
    ---------
    
    Co-authored-by: deepeshrai-tech <deepesh.rai@uipath.com>
    Co-authored-by: Naren <51352002+vnaren23@users.noreply.github.com>
    Co-authored-by: Raina451 <raina.middha@uipath.com>

--- src/services/data-fabric/entities.ts
+++ src/services/data-fabric/entities.ts
@@ -3,6 +3,7 @@ import type { IUiPath } from '../../core/types';
 import { EntityServiceModel, EntityGetResponse, createEntityWithMethods } from '../../models/data-fabric/entities.models';
 import {
   EntityGetRecordsByIdOptions,
+  EntityGetRecordByIdOptions,
   EntityInsertOptions,
   EntityBatchInsertOptions,
   EntityInsertResponse,
@@ -143,6 +144,47 @@ export class EntityService extends BaseService implements EntityServiceModel {
     }, options);
   }
 
+  /**
+   * Gets a single entity record by entity ID and record ID
+   *
+   * @param entityId - UUID of the entity
+   * @param recordId - UUID of the record
+   * @param options - Query options including expansionLevel
+   * @returns Promise resolving to the entity record
+   *
+   * @example
+   * ```typescript
+   * // Basic usage
+   * const record = await sdk.entities.getRecordById(<entityId>, <recordId>);
+   *
+   * // With expansion level
+   * const record = await sdk.entities.getRecordById(<entityId>, <recordId>, {
+   *   expansionLevel: 1
+   * });
+   * ```
+   */
+  @track('Entities.GetRecordById')
+  async getRecordById(
+    entityId: string,
+    recordId: string,
+    options: EntityGetRecordByIdOptions = {}
+  ): Promise<EntityRecord> {
+    const params = createParams({
+      expansionLevel: options.expansionLevel
+    });
+
+    const response = await this.get<EntityRecord>(
+      DATA_FABRIC_ENDPOINTS.ENTITY.GET_RECORD_BY_ID(entityId, recordId),
+      { params }
+    );
+
+    // Convert PascalCase response to camelCase
+    const camelResponse = pascalToCamelCaseKeys(response.data);
+    // Apply EntityMap transformations
+    const transformedResponse = transformData(camelResponse, EntityMap);
+    return transformedResponse;
+  }
+   
   /**
    * Inserts a single record into an entity by entity ID
    *
